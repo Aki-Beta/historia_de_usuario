5bcecdd4fac88cd46ce6a08987a938eb5ddc025b
Angela Garcia <angelagarciatorres1998@gmail.com>
2026-03-13 02:12:05 -0500

    add commit

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
+from products import *
+
 print("DAILY SALES RECORD")
-print("Type 'END' when you finish the day's record.")
+question()
+review_sales()
 
-
-# Resumen de Ventas
-print("\n=== RESUMEN DEL DÍA ===")
-print(f"{'Producto':<20} {'Cant.':<8} {'Precio U.':<10} {'Total':<10}")
-print("-" * 50)
-
-for producto, datos in ventas.items():
-    print(f"{producto:<20} {datos['cantidad']:<8} ${datos['precio_unitario']:<10.2f} ${datos['subtotal']:<10.2f}")
-
-print("-" * 50)
-print(f"{'TOTAL GENERAL':<38} ${total_general:.2f}")
+print("Thank you for visit my store, see you next time!")
