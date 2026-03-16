20b4158bc3f7adcf134b761661c90b5b4a47a60f
angela <angelagarciatorres1998@gmail.com>
2026-03-16 15:24:35 -0500

    añadir mejoras

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from products import *
 
 # This is the main module that runs the sales record application. It imports the functions from the products module and executes the main flow of the program.
 print("DAILY SALES RECORD")
-question()
+
 review_sales()
 
 # This line prints a thank you message to the user after the sales report has been generated.
--- products.py
+++ products.py
@@ -1,35 +1,56 @@
 products = []
-
+    
 # This module contains the functions to manage the products and sales records for a store.
-def add_product(name, price, quantity):
+def add_product(products):
+
+    product_name = input("Product name: ")
+
+    next = True
+    while next:
+        try: 
+            price = float(input("Price: "))
+            quantity = int(input("Quantity: "))
+            break
+
+        except ValueError:
+            print("invalid imput. please enter numeric value. ")
+            
+        
     product = {
-        'Product name': name,
+        'Product name': product_name,
         'Price': price,
         'Quantity': quantity
     }
-    products.append(product)
 
-# This function prompts the user to enter product details and adds them to the sales record.
-def question():
-    name = input("Product name: ")
-    price = float(input("Price: "))
-    quantity = int(input("Quantity: "))
+    products.append(product)
     
-    add_product(name, price, quantity)
-    
-    continuar = input("Do you want to add another product? (yes/no): ").strip().lower()
-    if continuar == "yes":
-        question()
 
 # This function generates a sales report based on the products added to the sales record.
 def review_sales():
+
+    products = []
+    next = "yes"
+
+    while next == "yes":
+        add_product(products)
+        next = input("Do you want to add another product? (yes/no): ").strip().lower()
+        
+        
     print("===================")
     print("Daily sales report")
+
     total = 0
+
     for product in products:
+            
         total_sales = product['Price'] * product['Quantity']
         total += total_sales
+
         print(f" Product name: {product['Product name']}")
-        print(f" Total quantity sold: {product['Quantity']}")
+        print(f"Price: ${product['Price']:}")
+        print(f"Total quantity sold: {product['Quantity']}")
+        print(f"Subtotal: ${total_sales:}")
+
     print(f" Total revenue: ${total: }")
+    
     print("===================")
